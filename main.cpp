3ee51c3561fcf1000f2afcbae0365cc0d4ab0425
LeXeL5 <lexel_2001@bk.ru>
2024-04-30 15:43:58 +0300

    Version 6.3

--- main.cpp
+++ main.cpp
@@ -9,14 +9,58 @@ struct Node {
 struct Tree {
 	int size = 0;
 	Node* root = nullptr;
-	int count() {
-		return size;
+
+	bool isLeaf(Node* node) {
+		if (!count()) return false;
+		if ((node->right == nullptr) and (node->left == nullptr)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
+	Node* goToParent(int value) {
+		Node* current = root;
+		Node* parent = nullptr;
+		while (current != nullptr) {
+			if (value == current->value) {
+				return parent;
+			}
+			else if (value < current->value) {
+				parent = current;
+				current = current->left;
+			}
+			else if (value > current->value) {
+				parent = current;
+				current = current->right;
+			}
+		}
+		return nullptr;
+	}
+	Node* goTo(int value) {
+		Node* current = root;
+		Node* parent = nullptr;
+		while (current != nullptr) {
+			if (value == current->value) {
+				return current;
+			}
+			else if (value < current->value) {
+				parent = current;
+				current = current->left;
+			}
+			else if (value > current->value) {
+				parent = current;
+				current = current->right;
+			}
+		}
+		return nullptr;
+	}
+
 	void add(int value) {
 		Node* newNode = new Node;
 		newNode->value = value;
 		size++;
-		if (count() == 1){
+		if (count() == 1) {
 			root = newNode;
 			return;
 		}
@@ -31,7 +75,7 @@ struct Tree {
 					return;
 				}
 			}
-			else {
+			else if (value > current->value) {
 				if (current->right != nullptr) {
 					current = current->right;
 				}
@@ -40,56 +84,125 @@ struct Tree {
 					return;
 				}
 			}
+			else if (value == current->value) {
+				delete newNode;
+				size--;
+				return;
+			}
 		}
 	}
-	/*void goDeeper(Node* current, int level) {
-		if (!count()) return;
-		level++;
-		if (current->left != nullptr) {
-			goDeeper(current->left, level);
+	void remove(int value) {
+		Node* deleteNodeParent = nullptr;
+		Node* deleteNode = root;
+		while (deleteNode != nullptr) {
+			if (deleteNode->value == value) {
+				break;
+			}
+			deleteNodeParent = deleteNode;
+			if (value < deleteNode->value) {
+				deleteNode = deleteNode->left;
+			}
+			else if (value > deleteNode->value) {
+				deleteNode = deleteNode->right;
+			}
 		}
-		if (current->right != nullptr) {
-			goDeeper(current->right, level);
+		while (true) {
+			if (deleteNode == nullptr) return;
+			if ((deleteNode->left == nullptr) and (deleteNode->right == nullptr)) {
+				if (deleteNodeParent != nullptr) {
+					if (deleteNodeParent->left == deleteNode) deleteNodeParent->left = nullptr;
+					if (deleteNodeParent->right == deleteNode) deleteNodeParent->right = nullptr;
+				}
+				if (root == deleteNode) root = nullptr;
+				delete deleteNode;
+				size--;
+				return;
+			}
+			else if ((deleteNode->left != nullptr) and (deleteNode->right == nullptr)) {
+				if (deleteNodeParent != nullptr) {
+					if (deleteNodeParent->left == deleteNode) deleteNodeParent->left = deleteNode->left;
+					if (deleteNodeParent->right == deleteNode) deleteNodeParent->right = deleteNode->left;
+				}
+				if (root == deleteNode) root = deleteNode->left;
+				delete deleteNode;
+				size--;
+				return;
+			}
+			else if ((deleteNode->left == nullptr) and (deleteNode->right != nullptr)) {
+				if (deleteNodeParent != nullptr) {
+					if (deleteNodeParent->left == deleteNode) deleteNodeParent->left = deleteNode->right;
+					if (deleteNodeParent->right == deleteNode) deleteNodeParent->right = deleteNode->right;
+				}
+				if (root == deleteNode) root = deleteNode->right;
+				delete deleteNode;
+				size--;
+				return;
+			}
+			else if ((deleteNode->left != nullptr) and (deleteNode->right != nullptr)) {
+				Node* current = deleteNode;
+				Node* currentParent = deleteNode;
+				current = current->right;
+				while (current->left != nullptr) {
+					currentParent = current;
+					current = current->left;
+				}
+				deleteNode->value = current->value;
+				deleteNodeParent = currentParent;
+				deleteNode = current;
+			}
 		}
-		cout << "Level: " << level << "\tValue: " << current->value << endl;
-		level--;
-	}*/
-	bool recursionContain(Node* current, int value) {
-		if (!count()) return false;
-		bool flag = false;
-		if (current->left != nullptr) {
-			flag = recursionContain(current->left, value);
+	}
+	void clear(Node* current) {
+		if (current == nullptr) return;
+		clear(current->left);
+		clear(current->right);
+		delete current;
+		size--;
+	}
+	void clear() {
+		clear(root);
+		root = nullptr;
+	}
+	bool contains(int value) {
+		if (goTo(value) != nullptr) {
+			return true;
 		}
-		if (current->right != nullptr) {
-			flag = recursionContain(current->right, value);
+		else {
+			return false;
 		}
-		if (current->value == value) {
-			flag = true;
-		}
-		return flag;
-	}/*
-	bool containst(Node* current, int level) {
-		if (!count()) return;
-		level++;
-		if (current->left != nullptr) {
-			goDeeper(current->left, level);
-		}
-		if (current->right != nullptr) {
-			goDeeper(current->right, level);
-		}
-		cout << "Level: " << level << "\tValue: " << current->value << endl;
-		level--;
-	}*/
+	}
+	int count() {
+		return size;
+	}
 };
 void main() {
 	Tree tree;
-	tree.add(3);
-	tree.add(2);
-	tree.add(1);
-	bool ooo = tree.recursionContain(tree.root, 3);
-	if (ooo) {
-		cout << "wow";
+	int input = 0;
+	cout << "Enter sequence of numbers (sequence end sign 0):" << endl;
+	cin >> input;
+	while (input) {
+		tree.add(input);
+		cin >> input;
 	}
 
-	//tree.goDeeper(tree.root, -1);
+	//убрать
+	cout << "Enter numbers to remove (sequence end sign 0):" << endl;
+	cin >> input;
+	while (input) {
+		tree.remove(input);
+		cin >> input;
+	}
+
+	cout << "Enter numbers to search (sequence end sign 0):" << endl;
+	cin >> input;
+	while (input) {
+		if (tree.contains(input)) {
+			cout << "This number is on the tree" << endl;
+		}
+		else {
+			cout << "This number is NOT on the tree" << endl;
+		}
+		cin >> input;
+	}
+	tree.clear();
 }
